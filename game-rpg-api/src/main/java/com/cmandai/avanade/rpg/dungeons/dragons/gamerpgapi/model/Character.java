5588cfabb05f842ea05522e66b4fe28ec536c26d
CamilaMandai <cayumandai@gmail.com>
2023-09-09 15:10:35 -0300

    feat: implementa entidade Character

---
+++ game-rpg-api/src/main/java/com/cmandai/avanade/rpg/dungeons/dragons/gamerpgapi/model/Character.java
@@ -0,0 +1,46 @@
+package com.cmandai.avanade.rpg.dungeons.dragons.gamerpgapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="character")
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class Character implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="name", nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(name = "life_points", nullable = false)
+    private Long lifePoints;
+
+    @Column(name = "strength", nullable = false)
+    private Long strength;
+
+    @Column(name = "defense", nullable = false)
+    private Long defense;
+
+    @Column(name = "agility", nullable = false)
+    private Long agility;
+
+    @Column(name = "dice_quantity", nullable = false)
+    private Integer diceQuantity;
+
+    @Column(name = "dice_sides", nullable = false)
+    private Integer diceSides;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "personality", nullable = false, length = 45)
+    private Personality personality;
+
+    public enum Personality {
+        HERO, MONSTER
+    }
+}

b2aee0f8a2ce7d97ce61c13738db18a7792d404c
CamilaMandai <cayumandai@gmail.com>
2023-09-10 18:27:18 -0300

    refactor: coloca Fighter na camada de model

--- game-rpg-api/src/main/java/com/cmandai/avanade/rpg/dungeons/dragons/gamerpgapi/model/Fighter.java
+++ game-rpg-api/src/main/java/com/cmandai/avanade/rpg/dungeons/dragons/gamerpgapi/model/Fighter.java
@@ -1,5 +1,4 @@
-package com.cmandai.avanade.rpg.dungeons.dragons.gamerpgapi.service.impl;
-import com.cmandai.avanade.rpg.dungeons.dragons.gamerpgapi.service.Fighter;
+package com.cmandai.avanade.rpg.dungeons.dragons.gamerpgapi.model;
 import com.cmandai.avanade.rpg.dungeons.dragons.gamerpgapi.model.Character;
 import com.cmandai.avanade.rpg.dungeons.dragons.gamerpgapi.utils.Dice;
 
@@ -8,34 +7,30 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter @NoArgsConstructor
-public class FighterImpl implements Fighter {
+public class Fighter {
 
     private Character character;
     private Long lifePoints;
 
-    public FighterImpl(Character character) {
+    public Fighter(Character character) {
         this.character = character;
         this.lifePoints = character.getLifePoints();
     }
 
-    @Override
     public Integer rollDiceToStart() {
         return Dice.roll(1,20);
     }
 
-    @Override
     public Long attack() {
         Long drawnValue = Dice.roll(1,12).longValue();
         return drawnValue + character.getStrength() + character.getAgility();
     }
 
-    @Override
     public Long defend() {
         Long drawnValue = Dice.roll(1,12).longValue();
         return drawnValue + character.getDefense() + character.getAgility();
     }
 
-    @Override
     public Long causeDamage() {
         Long diceDamage = Dice.roll(character.getDiceQuantity(),character.getDiceSides()).longValue();
         return diceDamage + character.getStrength();
--- game-rpg-api/src/main/java/com/cmandai/avanade/rpg/dungeons/dragons/gamerpgapi/service/Fighter.java
+++
@@ -1,8 +0,0 @@
-package com.cmandai.avanade.rpg.dungeons.dragons.gamerpgapi.service;
-
-public interface Fighter {
-    public Integer rollDiceToStart();
-    public Long attack();
-    public Long defend();
-    public Long causeDamage();
-}

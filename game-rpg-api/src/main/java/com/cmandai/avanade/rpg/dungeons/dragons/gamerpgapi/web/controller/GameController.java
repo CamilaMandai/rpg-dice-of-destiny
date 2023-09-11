074765a5a306cd65e58ec6222e526ac77f0d4bb7
CamilaMandai <cayumandai@gmail.com>
2023-09-11 10:46:14 -0300

    refactor: limpa endpoint play

--- game-rpg-api/src/main/java/com/cmandai/avanade/rpg/dungeons/dragons/gamerpgapi/web/controller/GameController.java
+++ game-rpg-api/src/main/java/com/cmandai/avanade/rpg/dungeons/dragons/gamerpgapi/web/controller/GameController.java
@@ -6,10 +6,15 @@ import com.cmandai.avanade.rpg.dungeons.dragons.gamerpgapi.service.CharacterServ
 import com.cmandai.avanade.rpg.dungeons.dragons.gamerpgapi.service.GameService;
 import com.cmandai.avanade.rpg.dungeons.dragons.gamerpgapi.web.dto.PlayRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,12 +24,11 @@ public class GameController {
     private final CharacterService characterService;
     private final GameService gameService;
     @PostMapping("play")
-    public Battle play(@RequestBody PlayRequestDto dto) {
+    public ResponseEntity<Battle> play(@RequestBody PlayRequestDto dto, UriComponentsBuilder uriBuilder) {
         Character playerCharacter = characterService.findById(dto.playerCharacterId());
-        if(dto.botCharacterId() == null){
-            return gameService.play(dto.playerName(), playerCharacter, null);
-        }
-        Character botCharacter = characterService.findById(dto.botCharacterId());
-        return gameService.play(dto.playerName(), playerCharacter, botCharacter);
+        Character botCharacter = (dto.botCharacterId() != null) ? characterService.findById(dto.botCharacterId()) : null;
+        Battle battle = gameService.play(dto.playerName(), playerCharacter, botCharacter);
+        URI uri = uriBuilder.path("battles/{id}").buildAndExpand(battle.getId()).toUri();
+        return ResponseEntity.created(uri).body(battle);
     };
 }
---
+++ game-rpg-api/src/main/java/com/cmandai/avanade/rpg/dungeons/dragons/gamerpgapi/web/dto/AttackResponseDTO.java
@@ -0,0 +1,4 @@
+package com.cmandai.avanade.rpg.dungeons.dragons.gamerpgapi.web.dto;
+
+public record AttackResponseDTO(Long attack) {
+}
